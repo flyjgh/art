31bfb7562fd65d55fd17f7e5cd15a7cecfa4614c
flyjgh <doctorzwei@gmail.com>
2021-10-07 03:59:58 +0200

    --

--- src/gif/attractor.jl
+++ src/gif/attractor.jl
@@ -56,9 +56,42 @@ function turnattract(frame_n, dotsize, w)
             if xx < xmax && xx > xmin && yy < ymax && yy > ymin
                 xpos = rescale(xx, xmin, xmax, -wover2, wover2)
                 ypos = rescale(yy, ymin, ymax, -wover2, wover2)
-                rcolor = rescale(zz, -1, 1, r(.1,.2),  r(.55, .9))
-                gcolor = rescale(yy, -1, 1, r(.1,.35), r(.45,.8))
+                rcolor = rescale(xx, -1, 1, r(.1,.2),  r(.55, .9))
+                gcolor = rescale(xx, -1, 1, r(.1,.35), r(.45,.8))
                 bcolor = rescale(xx, -1, 1, r(.2,.5),  r(.6,.9))
+                setcolor(convert(Colors.HSV, Colors.RGB(rcolor, gcolor, bcolor)))
+                circle(Point(xpos, ypos), dotsize, :fill)
+            end
+        end
+    end
+end
+
+function colorturn(frame_n, n::Int, k, l, dotsize, w)
+    xmin = -2.0; xmax = 2.0; ymin= -2.0; ymax = 2.0
+    origin()
+    background("black")
+    # control parameters
+    a = 2.2 + (frame_n / 800)
+    b = .95 + (frame_n / 600)
+    c = -.15 + (frame_n / 1000)
+    d = -2.2 + (frame_n / 4000)
+    e1 = 1.0
+    x = y = z = 0.0
+    wover2 = w / 2
+    for j in 1:w
+        for i in 1:w
+            xx = sin(a * y) - z  *  cos(b * x)
+            yy = z * sin(c * x) - cos(d * y)
+            zz = e1 * sin(x)
+            x = xx; y = yy; z = zz
+            if xx < xmax && xx > xmin && yy < ymax && yy > ymin
+                xpos = rescale(xx, xmin, xmax, -wover2, wover2)
+                ypos = rescale(yy, ymin, ymax, -wover2, wover2)
+                v = [xx, yy, zz]
+                h = v[n]; i = v[k]; j = v[l]
+                rcolor = rescale(h, -1, 1, r(.1,.2),  r(.55, .9))
+                gcolor = rescale(i, -1, 1, r(.1,.35), r(.45,.8))
+                bcolor = rescale(j, -1, 1, r(.2,.5),  r(.6,.9))
                 setcolor(convert(Colors.HSV, Colors.RGB(rcolor, gcolor, bcolor)))
                 circle(Point(xpos, ypos), dotsize, :fill)
             end
@@ -74,7 +107,7 @@ function turnmovie(w, nframes; fps=50)
         Scene(mv, backg, 0:nframes),
         Scene(mv, frame, 0:nframes)],
         framerate = fps,
-        pathname = "turnattract.gif",
+        pathname = "out/turnattract.gif",
         creategif = true)
 end
 
@@ -86,9 +119,21 @@ function colormovie(w, nframes; fps=50)
         Scene(mv, backg, 0:nframes),
         Scene(mv, frame, 0:nframes)],
         framerate = fps,
-        pathname = "colorattract.gif",
+        pathname = "out/colorattract.gif",
         creategif = true)
 end
 
-turnmovie(256, 400, fps=45)
+function colorturnmovie(w, nframes; fps=50)
+    mv = Movie(w, w, "mv")
+    backg(scene, frame_n) = background("black")
+    frame(scene, frame_n) = colorattract(frame_n, rand(1:3),rand(1:3),rand(1:3), r(.32, .42), w)
+    animate(mv, [
+        Scene(mv, backg, 0:nframes),
+        Scene(mv, frame, 0:nframes)],
+        framerate = fps,
+        pathname = "out/colorattract.gif",
+        creategif = true)
+end
+
+colorturnmovie(128, 500, fps=45)
 
---
+++ src/img/attract.jl
@@ -0,0 +1,52 @@
+using Luxor, Colors
+
+r(s, t) = rand(s:0.001:t)
+
+function strange(q, n, k, l, dotsize=.36, w=800.0)
+    xmin = -2.0; xmax = 2.0; ymin= -2.0; ymax = 2.0
+    Drawing(w, w, "./././out/strange-vector$q.png")
+    origin()
+    background("black")
+    xinc = w / (xmax - xmin)
+    yinc = w / (ymax - ymin)
+    # control parameters
+    a = r(2., 2.24)
+    # a = 2.24
+    b = r(.8, .95)
+    # b = .95
+    c = r(-.15, -.05)
+    # c = -.65
+    d = r(-3.,-2.2)
+    # d = -2.2
+    e1 = 1.0
+    x = y = z = 0.0
+    wover2 = w / 2
+    for j in 1:w
+        for i in 1:w
+            xx = sin(a * y) - z  *  cos(b * x)
+            yy = z * sin(c * x) - cos(d * y)
+            zz = e1 * sin(x)
+            x = xx; y = yy; z = zz
+            if xx < xmax && xx > xmin && yy < ymax && yy > ymin
+                xpos = rescale(xx, xmin, xmax, -wover2, wover2)
+                ypos = rescale(yy, ymin, ymax, -wover2, wover2)
+                v = [xx, yy, zz]
+                h = v[n]; o = v[k]; u = v[l]
+                rcolor = rescale(h, -1, 1, r(.0,.1), r(.5,.9))
+                gcolor = rescale(o, -1, 1, r(.05,.35), r(.3,.8))
+                bcolor = rescale(u, -1, 1, r(.1,.5), r(.6,.9))
+                setcolor(convert(Colors.HSV, Colors.RGB(rcolor, gcolor, bcolor)))
+                circle(Point(xpos, ypos), dotsize, :fill)
+            end
+        end
+    end
+    finish()
+end
+
+function save(n)
+    for c in 1:n
+        strange(c, rand(1:3),rand(1:3),rand(1:3), r(.32, .42), 200)
+    end
+end
+
+save(5)
