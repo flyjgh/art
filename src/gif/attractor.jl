40e37c50512547c643a090b3b5fafb9ab2005153
flyjgh <doctorzwei@gmail.com>
2021-10-10 20:01:29 +0200

    nw

--- src/gif/attractor.jl
+++ src/gif/attractor.jl
@@ -42,7 +42,7 @@ function colorattract(frame_n, n::Int, k, l, dotsize, w, ran)
     # control parameters
     a = 2.24
     b = .95 + (60 / frame_n)
-    c = -.15 + (frame_n / 600 + ran)
+    c = -.15 + frame_n / (600 + ran)
     d = -2.2
     e1 = 1.0
     x = y = z = 0.0
@@ -68,15 +68,15 @@ function colorattract(frame_n, n::Int, k, l, dotsize, w, ran)
     end
 end
 
-function turnattract(frame_n, dotsize, w, ran)
+function turnattract(frame_n, n, k, l, dotsize, w, ran)
     xmin = -2.0; xmax = 2.0; ymin= -2.0; ymax = 2.0
     origin()
     background("black")
     # control parameters
-    a = 2.2 + (frame_n / 800 + ran)
-    b = .95 + (frame_n / 600 + ran)
-    c = -.15 + (frame_n / 1000 + ran)
-    d = -2.2 + (frame_n / 3500 + ran)
+    a = 2.2 + frame_n / (800 + ran)
+    b = .95 + frame_n / (600 + ran)
+    c = -.15 + frame_n / (1000 + ran)
+    d = -2.2 + frame_n / (3500 + ran)
     e1 = 1.0
     x = y = z = 0.0
     wover2 = w / 2
@@ -89,9 +89,11 @@ function turnattract(frame_n, dotsize, w, ran)
             if xx < xmax && xx > xmin && yy < ymax && yy > ymin
                 xpos = rescale(xx, xmin, xmax, -wover2, wover2)
                 ypos = rescale(yy, ymin, ymax, -wover2, wover2)
-                rcolor = rescale(yy, -1, 1, r(.1,.2),  r(.55, .9))
-                gcolor = rescale(yy, -1, 1, r(.1,.35), r(.45,.8))
-                bcolor = rescale(yy, -1, 1, r(.2,.5),  r(.6,.9))
+                v = [xx, yy, zz]
+                h = v[n]; i = v[k]; j = v[l]
+                rcolor = rescale(h, -1, 1, r(.1,.2),  r(.55, .9))
+                gcolor = rescale(i, -1, 1, r(.1,.35), r(.45,.8))
+                bcolor = rescale(j, -1, 1, r(.2,.5),  r(.6,.9))
                 setcolor(convert(Colors.HSV, Colors.RGB(rcolor, gcolor, bcolor)))
                 circle(Point(xpos, ypos), dotsize, :fill)
             end
@@ -104,10 +106,10 @@ function colorturn(frame_n, n::Int, k, l, dotsize, w, ran)
     origin()
     background("black")
     # control parameters
-    a = 2.2 + (frame_n / 800 + ran)
-    b = .95 + (frame_n / 600 + ran)
-    c = -.15 + (frame_n / 1000 + ran)
-    d = -2.2 + (frame_n / 4000 + ran)
+    a = 2.2 + frame_n / (800 + ran)
+    b = .95 + frame_n / (600 + ran)
+    c = -.15 + frame_n / (1000 + ran)
+    d = -2.2 + frame_n / (4000 + ran)
     e1 = 1.0
     x = y = z = 0.0
     wover2 = w / 2
@@ -134,8 +136,9 @@ end
 
 function turnmovie(w, nframes, fps; name="turnattract", ran)
     mv = Movie(w, w, "mv")
+    a = rand(1:3); b = rand(1:3); c = rand(1:3)
     backg(scene, frame_n) = background("black")
-    frame(scene, frame_n) = turnattract(frame_n, r(.32, .42), w, ran)
+    frame(scene, frame_n) = turnattract(frame_n, a, b, c, r(.32, .42), w, ran)
     animate(mv, [
         Scene(mv, backg, 0:nframes),
         Scene(mv, frame, 0:nframes)],
@@ -147,7 +150,7 @@ end
 function interferencemovie(w, nframes, fps; name="turnattract", ran)
     mv = Movie(w, w, "mv")
     backg(scene, frame_n) = background("black")
-    frame(scene, frame_n) = interference(frame_n, rand(1:3),rand(1:3),rand(1:3), r(.32, .42), w, ran)
+    frame(scene, frame_n) = interference(frame_n, rand(1:3), rand(1:3), rand(1:3), r(.32, .42), w, ran)
     animate(mv, [
         Scene(mv, backg, 0:nframes),
         Scene(mv, frame, 0:nframes)],
@@ -159,7 +162,7 @@ end
 function colormovie(w, nframes, fps; name="colorattract", ran)
     mv = Movie(w, w, "mv")
     backg(scene, frame_n) = background("black")
-    frame(scene, frame_n) = colorattract(frame_n, rand(1:3),rand(1:3),rand(1:3), r(.32, .42), w, ran)
+    frame(scene, frame_n) = colorattract(frame_n, rand(1:3), rand(1:3), rand(1:3), r(.32, .42), w, ran)
     animate(mv, [
         Scene(mv, backg, 0:nframes),
         Scene(mv, frame, 0:nframes)],
@@ -171,7 +174,7 @@ end
 function colorturnmovie(w, nframes, fps; name="colorturnattract", ran)
     mv = Movie(w, w, "mv")
     backg(scene, frame_n) = background("black")
-    frame(scene, frame_n) = colorturn(frame_n, rand(1:3),rand(1:3),rand(1:3), r(.32, .42), w, ran)
+    frame(scene, frame_n) = colorturn(frame_n, rand(1:3), rand(1:3), rand(1:3), r(.32, .42), w, ran)
     animate(mv, [
         Scene(mv, backg, 0:nframes),
         Scene(mv, frame, 0:nframes)],
